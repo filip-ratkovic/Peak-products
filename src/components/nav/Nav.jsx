d9373ad62de0de594aa8513187471a8fa5fcab53
Filip Ratkovic <filip28r@gmail.com>
2024-10-02 11:01:03 +0200

    Nav bar and routes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import "./nav.css"
+import React from "react";
+import Logo from "../../images/logo.png"
+import "./nav.css";
+import { useNavigate } from "react-router";
 
 const Nav = () => {
+  const navigate = useNavigate()
   return (
-    <div>Nav</div>
-  )
-}
+    <main className="nav-main">
+      <section className="nav-logo-section">
+        <img 
+        onClick={()=> {navigate("/")}}
+        src={Logo} alt="Logo" />
+      </section>
 
-export default Nav
+      <section className="nav-profil-section">
+          <div className="nav-profil-cont">
+            prf
+          </div>
+          <div className="nav-profil-dark-cont">
+            dark
+          </div>
+      </section>
+    </main>
+  );
+};
+
+export default Nav;
