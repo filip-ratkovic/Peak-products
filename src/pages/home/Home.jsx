8f1e9100eab8c10f8b1d4e20281797bf211eb71f
Filip Ratkovic <filip28r@gmail.com>
2024-10-02 14:57:26 +0200

    Products api test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../containers/Layout'
 
 const Home = () => {
+  const [allProducts, setAllProducts] = useState('')
+
+useEffect(()=> {
+  fetch('https://dummyjson.com/products?limit=30&skip=30&select=title,price,images')
+  .then(res => res.json())
+  .then((data) => {
+    setAllProducts(data)
+  });
+},[])
+console.log(allProducts.products)
   return (
     <Layout>
       <div className="clssa">
-HOme
+        {allProducts.products.map((product)=> {
+       return (
+        <div> 
+             <h1>{product.title}</h1>
+             <p>{product.price}</p>
+             <img src={product.images[0]} alt={product.title} style={{width:"100px"}}/>
+        </div>
+       )
+        })}
       </div>
     </Layout>
   )
